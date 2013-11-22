8c0bb930595a50a12667a253df20c7bd2f5c9e57
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-11-22 16:53:08 -0500

    Update lpplmodel.py

--- lpplmodel.py
+++ lpplmodel.py
@@ -25,7 +25,7 @@ def lppl_costfunction(tarray, yarray, A=569.988, B=-266.943, C=-14.242,
                       tc=1930.218, phi=-4.1, omega=7.877, z=0.445):
     tyarray = zip(tarray, yarray)
     tyarray = filter(lambda item: item[0]<tc, tyarray)
-    filter_tarray = map(lambda item: item[0], tyarray)
-    filter_yarray = map(lambda item: item[1], tyarray)
+    filter_tarray = np.array(map(lambda item: item[0], tyarray))
+    filter_yarray = np.array(map(lambda item: item[1], tyarray))
     model = partial(lppl, A=A, B=B, C=C, tc=tc, phi=phi, omega=omega, z=z)
     return costfunction(filter_tarray, filter_yarray, model)
