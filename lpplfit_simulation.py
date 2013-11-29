17f865493658d15ceafc72fdb81762fd1311cb4b
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-11-29 12:46:26 -0500

    Update lpplfit_simulation.py

--- lpplfit_simulation.py
+++ lpplfit_simulation.py
@@ -11,20 +11,30 @@ from lpplmodel import lppl
 from lpplfit import LPPLGeneticAlgorithm
 import pylab
 
-def generate_simulated_data(tc, size=500, A=569.988, B=-266.943, C=-14.242,
+def generate_simulated_data(tc, lowt=None, hit=None,
+                            size=500, A=569.988, B=-266.943, C=-14.242,
                             phi=-4.1, omega=7.877, z=0.445):
-    tarray = np.array(sorted(np.random.uniform(low=tc-5, high=tc+1, size=size)))
+    if lowt == None:
+        lowt = tc-5
+    if hit == None:
+        hit = tc+1
+    tarray = np.array(sorted(np.random.uniform(low=lowt, high=hit, size=size)))
     critical_stock = lppl(tc-0.05, A=A, B=B, C=C, tc=tc, phi=phi, omega=omega, z=z)
     fnc = lambda t: lppl(t, A=A, B=B, C=C, tc=tc, phi=phi, omega=omega, z=z) if t<tc else critical_stock*np.random.uniform()
     yarray = np.array(map(fnc, tarray))
     return tarray, yarray
     
-def simulate(tc, size=500, A=569.988, B=-266.943, C=-14.242, phi=-4.1, 
-             omega=7.877, z=0.445, max_iter=1000, param_popsize=50):
+def simulate(tc, lowt=None, hit=None, size=500, A=569.988, B=-266.943, 
+             C=-14.242, phi=-4.1, omega=7.877, z=0.445, max_iter=1000, 
+             param_popsize=100):
     fitalg = LPPLGeneticAlgorithm()
     
-    tarray, yarray = generate_simulated_data(tc, size=size, A=A, B=B, C=C,
-                                             phi=phi, omega=omega, z=z)
+    tarray, yarray = generate_simulated_data(tc, lowt=lowt, hit=hit, size=size,
+                                             A=A, B=B, C=C, phi=phi, 
+                                             omega=omega, z=z)
+    tyarray = filter(lambda item: item[0]<tc-0.5, zip(tarray, yarray))
+    tarray = np.array(map(lambda item: item[0], tyarray))
+    yarray = np.array(map(lambda item: item[1], tyarray))
     
     param_pop = fitalg.generate_init_population(tarray, size=param_popsize)
     costs_iter = [fitalg.lpplcostfunc(tarray, yarray, param_pop[0])]
@@ -43,9 +53,9 @@ def simulate(tc, size=500, A=569.988, B=-266.943, C=-14.242, phi=-4.1,
     print res_param
     print 'cost = ', fitalg.lpplcostfunc(tarray, yarray, res_param)
     
-    pylab.plot(tarray, yarray)
-    pylab.plot(tarray, fitalg.lppl(tarray, res_param))
+    pylab.plot(tarray, yarray, 'k')
+    pylab.plot(tarray, fitalg.lppl(tarray, res_param), 'b')
     pylab.savefig('theory_sim.png')
     
 if __name__ == '__main__':
-    simulate(1930, size=500, max_iter=200)
+    simulate(2013, lowt=2010, hit=2012, size=500, max_iter=90)
