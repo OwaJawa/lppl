9f2f29bac45bdc2db1722353bb1e36f911ef618b
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-11-29 13:55:32 -0500

    Update batchLPPLsim.py

--- batchLPPLsim.py
+++ batchLPPLsim.py
@@ -8,6 +8,7 @@ Created on Fri Nov 29 12:52:41 2013
 from lpplfit import LPPLGeneticAlgorithm
 from lppl_datageneration import generate_simulated_data
 #import numpy as np
+import csv
 
 fitalg = LPPLGeneticAlgorithm()
 
@@ -24,5 +25,37 @@ def dat_simulate(tc, lowt=None, hit=None, size=500, A=569.988, B=-266.943,
     cost = fitalg.lpplcostfunc(tarray, yarray, resparam)
     return resparam, cost
     
+def simulation1(outputfilename):
+    header = ['ID', 'mutprob', 'reproduceprob', 'max_iter', 'param_popsize',
+              'lowt', 'hit', 'tc', 'fit tc']
+    f = open(outputfilename, 'wb')
+    writer = csv.writer(f)
+    writer.writerow(header)
+    i = 0
+    mutprobs = [0.25, 0.5, 0.75]
+    reproduceprobs = [0.25, 0.5, 0.75]
+    max_iters = [50, 100, 150]
+    param_popsizes = [50, 100, 500]
+    tranges = [(2010, 2012), (2011, 2012), (2010, 2012.5), (2011, 2012.5)]
+    for trange in tranges:
+        for max_iter in max_iters:
+            for mutprob in mutprobs:
+                for reproduceprob in reproduceprobs:
+                    for param_popsize in param_popsizes:
+                        print 'Doing simulation ', i
+                        resparam, cost = dat_simulate(2013, lowt=trange[0],
+                                                      hit=trange[1], size=500,
+                                                      mutprob=mutprob,
+                                                      reproduceprob=reproduceprob,
+                                                      max_iter=max_iter,
+                                                      param_popsize=param_popsize)
+                        writer.writerow([i, mutprob, reproduceprob, max_iter,
+                                         param_popsize, trange[0], trange[1],
+                                         2013, resparam['tc']])
+                        i += 1
+    f.close()
+        
+    
 if __name__ == '__main__':
-    print dat_simulate(2013, lowt=2010, hit=2012)
+    #print dat_simulate(2013, lowt=2010, hit=2012)
+    simulation1('simulation1.csv')
