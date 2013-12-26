09153cbc489675172194d0a528697540bb9f7139
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-12-25 19:09:18 -0500

    Change argument parser

--- lppl_fitdata.py
+++ lppl_fitdata.py
@@ -6,8 +6,8 @@ Created on Tue Dec 24 16:47:38 2013
 """
 
 from lpplfit import LPPLGeneticAlgorithm
-import sys
 import numpy as np
+import argparse
 
 fitalg = LPPLGeneticAlgorithm()
 
@@ -18,25 +18,38 @@ def readData(filename, header=False):
     (tarray, yarray) = data
     return tarray, yarray
 
-def help():
-    print 'Arguments: <filename>'
+prog_descp = 'Read the financial data from the input file, '
+prog_descp += 'the first column being the year and second column prices, '
+prog_descp += 'and then fit the data to estimate the time of the bubble crash.'
     
 if __name__ == '__main__':
-    argvs = sys.argv
-    if len(argvs) < 2:
-        help()
-    else:
-        filename = argvs[1]
-        tarray, yarray = readData(filename)
-        param_pop, costs_iter = fitalg.perform(tarray, yarray)
-        res_param = fitalg.grad_optimize(tarray, yarray, param_pop[0])
-        
-        print 'Results: '
-        print 'A = ', res_param['A']
-        print 'B = ', res_param['B']
-        print 'C = ', res_param['C']
-        print 'phi = ', res_param['phi']
-        print 'omega = ', res_param['omega']
-        print 'z = ', res_param['z']
-        print 'tc = ', res_param['tc']
+    argv_parser = argparse.ArgumentParser(description=prog_descp)
+    argv_parser.add_argument('filename',
+                             help='the name of the file for fitting')
+    argv_parser.add_argument('--parampopsize', type=int, default=500,
+                             help='number of sets of parameters (default=500)')
+    argv_parser.add_argument('--maxiter', type=int, default=150,
+                             help='maximum number of iterations of genetic algorithm (default=150)')
+    argv_parser.add_argument('--mutprob', type=float, default=0.75,
+                             help='mutation probability (between 0 and 1, default=0.75)')
+    argv_parser.add_argument('--reproduceprob', type=float, default=0.25,
+                             help='breeding probability (between 0 and 1, default=0.25)')
+    args = argv_parser.parse_args()
+    
+    tarray, yarray = readData(args.filename)
+    param_pop, costs_iter = fitalg.perform(tarray, yarray, 
+                                           size=args.parampopsize,
+                                           max_iter=args.maxiter,
+                                           mutprob=args.mutprob,
+                                           reproduceprob=args.reproduceprob)
+    res_param = fitalg.grad_optimize(tarray, yarray, param_pop[0])
+    
+    print 'Results: '
+    print 'A = ', res_param['A']
+    print 'B = ', res_param['B']
+    print 'C = ', res_param['C']
+    print 'phi = ', res_param['phi']
+    print 'omega = ', res_param['omega']
+    print 'z = ', res_param['z']
+    print 'tc = ', res_param['tc']
         
