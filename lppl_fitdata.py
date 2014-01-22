72249358233c5744993d3494c7cb0fd8b1bd0961
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-01-21 19:11:12 -0500

    Incorporate datetime handling

--- lppl_fitdata.py
+++ lppl_fitdata.py
@@ -8,8 +8,12 @@ Created on Tue Dec 24 16:47:38 2013
 from lpplfit import LPPLGeneticAlgorithm
 import numpy as np
 import argparse
+from datetime import datetime as dt
 
-fitalg = LPPLGeneticAlgorithm()
+def toYearFraction(year, month, day):
+    wholeyear = dt(year+1, 1, 1).toordinal() - dt(year, 1, 1).toordinal()
+    fraction = dt(year, month, day).toordinal() - dt(year, 1, 1).toordinal()
+    return year + float(fraction)/float(wholeyear)
 
 def readData(filename, header=False):
     data = np.loadtxt(filename, unpack=True, skiprows=1 if header else 0,
@@ -18,11 +22,10 @@ def readData(filename, header=False):
     (tarray, yarray) = data
     return tarray, yarray
 
-prog_descp = 'Read the financial data from the input file, '
-prog_descp += 'the first column being the year and second column prices, '
-prog_descp += 'and then fit the data to estimate the time of the bubble crash.'
-    
-if __name__ == '__main__':
+def get_argvparser():
+    prog_descp = 'Read the financial data from the input file, '
+    prog_descp += 'the first column being the year and second column prices, '
+    prog_descp += 'and then fit the data to estimate the time of the bubble crash.'
     argv_parser = argparse.ArgumentParser(description=prog_descp)
     argv_parser.add_argument('filename',
                              help='the name of the file for fitting')
@@ -34,9 +37,15 @@ if __name__ == '__main__':
                              help='mutation probability (between 0 and 1, default=0.75)')
     argv_parser.add_argument('--reproduceprob', type=float, default=0.25,
                              help='breeding probability (between 0 and 1, default=0.25)')
+    return argv_parser
+    
+if __name__ == '__main__':
+    argv_parser = get_argvparser()
     args = argv_parser.parse_args()
     
     tarray, yarray = readData(args.filename)
+    
+    fitalg = LPPLGeneticAlgorithm()
     param_pop, costs_iter = fitalg.perform(tarray, yarray, 
                                            size=args.parampopsize,
                                            max_iter=args.maxiter,
