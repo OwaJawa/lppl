81f1f6f42a3492af6781d0ffef025691a995d1a9
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-03-04 15:06:08 -0500

    multi-threaded fitting, still problems

--- lpplfit.py
+++ lpplfit.py
@@ -7,7 +7,9 @@ Created on Wed Nov 20 15:53:45 2013
 
 import numpy as np
 from scipy.optimize import minimize
-from lpplmodel import lppl, lppl_costfunction
+from lpplmodel import lpplcostfunc_dictparam
+from multiprocessing import Pool
+from operator import add
 
 class InconsistentParameterSizeException(Exception):
     def __init__(self, num1, num2):
@@ -16,11 +18,10 @@ class InconsistentParameterSizeException(Exception):
         self.message = 'Inconsistent parameter size: '+str(num1)+' vs. '+str(num2)
 
 class LPPLGeneticAlgorithm:
-    def __init__(self, guesstc=1980):
+    def __init__(self):
         self.meanA = 600
         self.meanB = -250
         self.meanC = -20
-        self.meantc = guesstc
         self.stdA = 50
         self.stdB = 50
         self.stdC = 25
@@ -78,17 +79,6 @@ class LPPLGeneticAlgorithm:
             init_parameters_pop.append(parameters)
         return init_parameters_pop
 
-    def lppl(self, tarray, parameters):
-        return lppl(tarray, A=parameters['A'], B=parameters['B'],
-                    C=parameters['C'], tc=parameters['tc'], 
-                    phi=parameters['phi'], omega=parameters['omega'],
-                    z=parameters['z'])        
-
-    def lpplcostfunc(self, tarray, yarray, parameters):
-        return lppl_costfunction(tarray, yarray, A=parameters['A'], 
-                                 B=parameters['B'], C=parameters['C'], 
-                                 tc=parameters['tc'], phi=parameters['phi'],
-                                 omega=parameters['omega'], z=parameters['z'])
                        
     def mutate(self, parameters, tarray, yarray):
         mut_parameters = {}
@@ -161,7 +151,7 @@ class LPPLGeneticAlgorithm:
         return offsprings
         
     def cull_population(self, parameters_pop, tarray, yarray, size=500):
-        costs = map(lambda param: self.lpplcostfunc(tarray, yarray, param),
+        costs = map(lambda param: lpplcostfunc_dictparam(tarray, yarray, param),
                     parameters_pop)
         param_cost_pairs = zip(parameters_pop, costs)
         param_cost_pairs = filter(lambda pair: not np.isnan(pair[1]),
@@ -193,24 +183,24 @@ class LPPLGeneticAlgorithm:
                                                          len(init_param_pop))
             param_pop = init_param_pop
                 
-        costs_iter = [self.lpplcostfunc(tarray, yarray, param_pop[0])]
+        costs_iter = [lpplcostfunc_dictparam(tarray, yarray, param_pop[0])]
         for i in range(max_iter):
             param_pop = self.reproduce(tarray, yarray, param_pop, size=size, 
                                        mutprob=mutprob,
                                        reproduceprob=reproduceprob)
-            cost = self.lpplcostfunc(tarray, yarray, param_pop[0])
+            cost = lpplcostfunc_dictparam(tarray, yarray, param_pop[0])
             costs_iter.append(cost)
         return param_pop, costs_iter
         
     def grad_optimize(self, tarray, yarray, parameters):
-        costfunc = lambda paramarray: self.lpplcostfunc(tarray, yarray,
-                                                        {'A': paramarray[0],
-                                                         'B': paramarray[1],
-                                                         'C': paramarray[2],
-                                                         'tc': paramarray[3],
-                                                         'phi': paramarray[4],
-                                                         'omega': paramarray[5],
-                                                         'z': paramarray[6]})
+        costfunc = lambda paramarray: lpplcostfunc_dictparam(tarray, yarray,
+                                                             {'A': paramarray[0],
+                                                              'B': paramarray[1],
+                                                             'C': paramarray[2],
+                                                             'tc': paramarray[3],
+                                                             'phi': paramarray[4],
+                                                             'omega': paramarray[5],
+                                                             'z': paramarray[6]})
         init_param_array = np.array([parameters['A'], parameters['B'],
                                      parameters['C'], parameters['tc'],
                                      parameters['phi'], parameters['omega'],
@@ -220,3 +210,39 @@ class LPPLGeneticAlgorithm:
                        'tc': res.x[3], 'phi': res.x[4], 'omega': res.x[5],
                        'z': res.x[6]}
         return final_param
+
+class PoolLPPLGeneticAlgorithm(LPPLGeneticAlgorithm):
+    def __init__(self, numthreads=1):
+        LPPLGeneticAlgorithm.__init__(self)
+        self.numthreads = numthreads
+        
+    def mutate_population(self, parameters_pop, tarray, yarray, mutprob=0.75):
+        numperthread = int(np.ceil(float(len(parameters_pop))/self.numthreads))
+        param_pop_lists = [parameters_pop[idx:min(idx, len(parameters_pop))] for idx in range(0, len(parameters_pop), numperthread)]
+        mutation_workers = Pool(processes=self.numthreads)
+        mut_param_pop_lists = mutation_workers.map(lambda param_pop: super(PoolLPPLGeneticAlgorithm, self).mutate_population(param_pop, tarray, yarray, mutprob=mutprob),
+                                                   param_pop_lists)
+        return reduce(add, mut_param_pop_lists)
+        
+    def breed_population(self, parameters_pop, tarray, yarray,
+                         reproduceprob=0.25):
+        breed_workers = Pool(processes=self.numthreads)
+        breed_param_pop_lists = breed_workers.map(lambda breed_prob: super(PoolLPPLGeneticAlgorithm, self).breed_population(parameters_pop, tarray, yarray, reproduceprob=breed_prob),
+                                                  [(reproduceprob/self.numthreads) for i in range(self.numthreads)])
+        return reduce(add, breed_param_pop_lists)
+        
+    def cull_population(self, parameters_pop, tarray, yarray, size=500):
+        numperthread = int(np.ceil(float(len(parameters_pop))/self.numthreads))
+        param_pop_lists = [parameters_pop[idx:min(idx, len(parameters_pop))] for idx in range(0, len(parameters_pop), numperthread)]
+        culler = Pool(processes=self.numthreads)
+        costs_lists = culler.map(lambda param_pop: self.lpplcostfunc(tarray, yarray, param_pop),
+                                 param_pop_lists)
+        costs = reduce(add, costs_lists)
+        param_cost_pairs = zip(parameters_pop, costs)
+        param_cost_pairs = filter(lambda pair: not np.isnan(pair[1]),
+                                  param_cost_pairs)
+        param_cost_pairs = sorted(param_cost_pairs, key=lambda item: item[1])
+        if (len(param_cost_pairs) < size):
+            return map(lambda item: item[0], param_cost_pairs)
+        else:
+            return map(lambda item: item[0], param_cost_pairs[0:size])
